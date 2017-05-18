cde76538956fc5b6dfd398a88473710e711de3ca
mathewcmartin <chactasstryker@gmail.com>
2017-05-18 16:33:36 -0700

    Dried code and finished todos

--- starter-code/server.js
+++ starter-code/server.js
@@ -17,7 +17,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = 'postgres://localhost:5432';// TODO: Don't forget to set your own conString
+const conString = 'postgres://postgres:1234@localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
@@ -50,7 +50,7 @@ app.get('/articles', function(request, response) {
 app.post('/articles', function(request, response) {
   client.query(
   `INSERT INTO
-  articles(author, "authorUrl")
+  authors(author, "authorUrl")
   VALUES ($1, $2)
   ON CONFLICT DO NOTHING;
   `, // TODO: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
@@ -87,15 +87,14 @@ app.post('/articles', function(request, response) {
   function queryThree(author_id) {
     client.query(
       `INSERT INTO articles
-      (author_id)`, // TODO: Write a SQL query to insert the new article using the author_id from our previous query
+      (author_id, title, category, "publishedOn", body)
+      VALUES ($1, $2, $3, $4, $5)`, // TODO: Write a SQL query to insert the new article using the author_id from our previous query
       [
+        author_id,
         request.body.title,
-        request.body.author,
-        request.body.authorUrl,
         request.body.category,
         request.body.publishedOn,
-        request.body.body,
-        request.body.author_id
+        request.body.body
       ], // TODO: Add the data from our new article, including the author_id, as data for the SQL query.
       function(err) {
         if (err) console.error(err);
@@ -138,7 +137,7 @@ app.put('/articles/:id', function(request, response) {
         request.body.category,
         request.body.publishedOn,
         request.body.body,
-        request.params.id
+        request.body.author_id
       ]
     )
   })
